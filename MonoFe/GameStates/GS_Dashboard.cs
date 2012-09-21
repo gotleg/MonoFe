d9614a74f35587431ac7249b0c56c12a784fea29
Gotleg <gotleg@gmail.com>
2012-09-21 22:50:43 +0200

    Menu evolution

--- MonoFe/GameStates/GS_Dashboard.cs
+++ MonoFe/GameStates/GS_Dashboard.cs
@@ -15,6 +15,7 @@ namespace MonoFe.GameStates
 	public class GS_Dashboard :IGameState
 	{
 		SdlDotNet.Graphics.Font fontXXSmall, fontXSmall,fontSmall, fontNormal, fontMedium, fontLarge, fontXL, fontXXL;
+		SdlDotNet.Graphics.Font font8, font10,font12, font14, font16, font18, font20, font24,font28,font32,font36,font40;
 		double animationSpeed = Convert.ToDouble (ConfigurationSettings.AppSettings ["StarsAnimationSpeed"]);
 		int currentEmulator;
 		string[] emulators = new string[2];
@@ -32,12 +33,14 @@ namespace MonoFe.GameStates
 		Surface m_FontShadowSurface;
 		Surface sfError;
 		Surface sfErrorBackground;
+		Surface sfNbRoms;
 		List<Game> gameList;
 		List<Surface> itemList = new List<Surface> ();
 		Sound changeGame ;
 		Sound changeEmulator;
 		Sound music;
-		int[] itemY = new int[15];
+		int[] itemX = new int[21];
+		int[] itemY = new int[21];
 		bool displayError;
 		string errorMessage = "";
 		float[] starsX = new float[512];
@@ -52,7 +55,7 @@ namespace MonoFe.GameStates
 		//List<Surface> stars = new List<Surface>();
 		List<ParticlePixel> lstPixels = new List<ParticlePixel> ();
 		int currentIndex;
-		int displayedItems = 15;
+		int displayedItems = 21;
 		int startX = 20;
 		int startY = 300;
 		int lineSpacing = 40;
@@ -68,21 +71,73 @@ namespace MonoFe.GameStates
 		public void Init ()
 		{
 			//Init position of items in list
+			/*
 			itemY[0] = startY;
-			itemY[1] = startY+12;
-			itemY[2] = startY+12+15;
-			itemY[3] = startY+12+15+18;
-			itemY[4] = startY+12+15+18+22;
-			itemY[5] = startY+12+15+18+22+26;
-			itemY[6] = startY+12+15+18+22+26+34;
-			itemY[7] = startY+12+15+18+22+26+34+44;
-			itemY[8] = startY+12+15+18+22+26+34+44+44+12;
-			itemY[9] = startY+12+15+18+22+26+34+44+44+12+38;
-			itemY[10] = startY+12+15+18+22+26+34+44+44+12+38+30;
-			itemY[11] = startY+12+15+18+22+26+34+44+44+12+38+30+24;
-			itemY[12] = startY+12+15+18+22+26+34+44+44+12+38+30+24+18;
-			itemY[13] = startY+12+15+18+22+26+34+44+44+12+38+30+24+18+16;
-			itemY[14] = startY+12+15+18+22+26+34+44+44+12+38+30+24+18+16+14;
+			itemY[1] = startY+12;//+12
+			itemY[2] = startY+27;//+15
+			itemY[3] = startY+42;//+15
+			itemY[4] = startY+60;//+18
+			itemY[5] = startY+80;//+20
+			itemY[6] = startY+102;//+22
+			itemY[7] = startY+126;//+24
+			itemY[8] = startY+152;//+26
+			itemY[9] = startY+184;//+32
+			itemY[10] = startY+220;//+36 // center item
+			itemY[11] = startY+266;//+46
+			itemY[12] = startY+302;//+36
+			itemY[13] = startY+334;//+32
+			itemY[14] = startY+360;//+26
+			itemY[15] = startY+384;//+24
+			itemY[16] = startY+406;//+22
+			itemY[17] = startY+426;//+20
+			itemY[18] = startY+444;//+18
+			itemY[19] = startY+459;//+15
+			itemY[20] = startY+471;//+12
+			*/
+
+			itemY[0] = startY;
+			itemY[1] = startY+16;//+16
+			itemY[2] = startY+35;//+19
+			itemY[3] = startY+54;//+19
+			itemY[4] = startY+76;//+22
+			itemY[5] = startY+100;//+24
+			itemY[6] = startY+126;//+26
+			itemY[7] = startY+154;//+28
+			itemY[8] = startY+184;//+30
+			itemY[9] = startY+220;//+36
+			itemY[10] = startY+260;//+40 // center item
+			itemY[11] = startY+310;//+50
+			itemY[12] = startY+350;//+40
+			itemY[13] = startY+386;//+36
+			itemY[14] = startY+416;//+30
+			itemY[15] = startY+444;//+28
+			itemY[16] = startY+470;//+26
+			itemY[17] = startY+494;//+24
+			itemY[18] = startY+516;//+22
+			itemY[19] = startY+535;//+19
+			itemY[20] = startY+554;//+19
+
+			itemX[0] = startX;
+			itemX[1] = startX+3;
+			itemX[2] = startX+6;
+			itemX[3] = startX+9;
+			itemX[4] = startX+12;
+			itemX[5] = startX+15;
+			itemX[6] = startX+18;
+			itemX[7] = startX+21;
+			itemX[8] = startX+24;
+			itemX[9] = startX+27;
+			itemX[10] = startX+30;// center item
+			itemX[11] = startX+27;
+			itemX[12] = startX+24;
+			itemX[13] = startX+21;
+			itemX[14] = startX+18;
+			itemX[15] = startX+15;
+			itemX[16] = startX+12;
+			itemX[17] = startX+9;
+			itemX[18] = startX+6;
+			itemX[19] = startX+3;
+			itemX[20] = startX;
 
 			sfError = new Surface (new Size (1, 1));
 			sfErrorBackground = new Surface (new Size (1, 1));
@@ -124,6 +179,19 @@ namespace MonoFe.GameStates
 			fontLarge = new SdlDotNet.Graphics.Font (ConfigurationSettings.AppSettings ["FontFile"], 24);
 			fontXL = new SdlDotNet.Graphics.Font (ConfigurationSettings.AppSettings ["FontFile"], 32);
 			fontXXL = new SdlDotNet.Graphics.Font (ConfigurationSettings.AppSettings ["FontFile"], 44);
+
+			font8 = new SdlDotNet.Graphics.Font (ConfigurationSettings.AppSettings ["FontFile"], 8);
+			font10 = new SdlDotNet.Graphics.Font (ConfigurationSettings.AppSettings ["FontFile"], 10);
+			font12 = new SdlDotNet.Graphics.Font (ConfigurationSettings.AppSettings ["FontFile"], 12);
+			font14 = new SdlDotNet.Graphics.Font (ConfigurationSettings.AppSettings ["FontFile"], 14);
+			font16 = new SdlDotNet.Graphics.Font (ConfigurationSettings.AppSettings ["FontFile"], 16);
+			font18 = new SdlDotNet.Graphics.Font (ConfigurationSettings.AppSettings ["FontFile"], 18);
+			font20 = new SdlDotNet.Graphics.Font (ConfigurationSettings.AppSettings ["FontFile"], 20);
+			font24 = new SdlDotNet.Graphics.Font (ConfigurationSettings.AppSettings ["FontFile"], 24);
+			font28 = new SdlDotNet.Graphics.Font (ConfigurationSettings.AppSettings ["FontFile"], 28);
+			font32 = new SdlDotNet.Graphics.Font (ConfigurationSettings.AppSettings ["FontFile"], 32);
+			font36 = new SdlDotNet.Graphics.Font (ConfigurationSettings.AppSettings ["FontFile"], 36);
+			font40 = new SdlDotNet.Graphics.Font (ConfigurationSettings.AppSettings ["FontFile"], 40);
 			//init starfield
 			for (int i=0; i<512; i++) {
 				starsX [i] = rand.Next (-1000, 1000);
@@ -203,6 +271,13 @@ namespace MonoFe.GameStates
 				sfPlatefromName = new Surface (ConfigurationSettings.AppSettings ["SnesLogo"]);
 				break;
 			}
+			string txtNbRoms = "";
+			if (gameList.Count <= 1)
+				txtNbRoms = Convert.ToString(gameList.Count) + " rom";
+			else
+				txtNbRoms = Convert.ToString(gameList.Count) + " roms";
+			sfNbRoms = new Surface(font24.Render(txtNbRoms,Color.White));
+			ScreenManager.MainScreen.Blit (sfNbRoms, new Point (((ScreenManager.MainScreen.Width - sfNbRoms.Width) / 2), 250));
 			//sfPlatefromName = fontXXL.Render (emulators [currentEmulator], Color.White);
 			ScreenManager.MainScreen.Blit (sfPlatefromName, new Point (Convert.ToInt32 ((ScreenManager.MainScreen.Width - sfPlatefromName.Width) / 2), 150));
 
@@ -214,44 +289,53 @@ namespace MonoFe.GameStates
 
 			//items vides début
 			int emptyLines = 0;
-			if (currentIndex < 7) {
-				for (int i=0; i<7-currentIndex; i++) {
+			if (currentIndex < 10) {
+				for (int i=0; i<10-currentIndex; i++) {
 					switch (itemList.Count)
 					{
 					case 0:
-						itemList.Add (fontXXSmall.Render (" ", Color.White));
+						itemList.Add (font8.Render (" ", Color.White));
 						break;
 					case 1:
-						itemList.Add (fontXSmall.Render (" ", Color.White));
+						itemList.Add (font10.Render (" ", Color.White));
 						break;
 					case 2:
-						itemList.Add (fontSmall.Render ("", Color.White));
+						itemList.Add (font12.Render (" ", Color.White));
 						break;
 					case 3:
-						itemList.Add (fontNormal.Render ("", Color.White));
+						itemList.Add (font14.Render (" ", Color.White));
 						break;
 					case 4:
-						itemList.Add (fontMedium.Render ("", Color.White));
+						itemList.Add (font16.Render (" ", Color.White));
 						break;
 					case 5:
-						itemList.Add (fontLarge.Render ("", Color.White));
+						itemList.Add (font18.Render (" ", Color.White));
 						break;
 					case 6:
-						itemList.Add (fontXL.Render ("", Color.White));
+						itemList.Add (font20.Render (" ", Color.White));
+						break;
+					case 7:
+						itemList.Add (font24.Render (" ", Color.White));
+						break;
+					case 8:
+						itemList.Add (font28.Render (" ", Color.White));
+						break;
+					case 9:
+						itemList.Add (font32.Render (" ", Color.White));
 						break;
 					}
 
-					ScreenManager.MainScreen.Blit (itemList [itemList.Count - 1], new Point (startX, (itemY[itemList.Count - 1])));
+					ScreenManager.MainScreen.Blit (itemList [itemList.Count - 1], new Point (itemX[itemList.Count - 1], itemY[itemList.Count - 1]));
 					emptyLines++;
 				}
 			}
 			foreach (Game g in gameList) {
 
-				//We need to show 9 products
+				//We need to show 21 products
 				//Selected product is always in the middle of the list
-				// We can have 4 empty items when at the begining or the end of the list
-				//So we display 4 items in top of the selected item and 4 at the bottom
-				if (currentGame >= currentIndex - 7 && currentGame < (currentIndex + 8)) {
+				// We can have 10 empty items when at the begining or the end of the list
+				//So we display 10 items in top of the selected item and 10 at the bottom
+				if (currentGame >= currentIndex - 10 && currentGame < (currentIndex + 11)) {
 					if (currentGame == currentIndex) {
 						//Selected Item
 						string hexStarsColors = ConfigurationSettings.AppSettings ["HexaRGBSelectedItemColor"];
@@ -259,14 +343,14 @@ namespace MonoFe.GameStates
 						string hexSelectedItemG = hexStarsColors.Substring (2, 2);
 						string hexSelectedItemB = hexStarsColors.Substring (4, 2);
 						
-						itemList.Add (fontXXL.Render (g.gameName, Color.FromArgb (int.Parse (hexSelectedItemR, System.Globalization.NumberStyles.HexNumber), int.Parse (hexSelectedItemG, System.Globalization.NumberStyles.HexNumber), int.Parse (hexSelectedItemB, System.Globalization.NumberStyles.HexNumber))));
+						itemList.Add (font40.Render (g.gameName, Color.FromArgb (int.Parse (hexSelectedItemR, System.Globalization.NumberStyles.HexNumber), int.Parse (hexSelectedItemG, System.Globalization.NumberStyles.HexNumber), int.Parse (hexSelectedItemB, System.Globalization.NumberStyles.HexNumber))));
 						sfGameName = fontXL.Render (g.gameName, Color.White);
 						sfGameGenre = fontNormal.Render (g.genre, Color.White);
 						sfGameDeveloperYear = fontNormal.Render (g.developer + " " + g.year, Color.White);
 						ScreenManager.MainScreen.Blit (sfGameName, new Point (ScreenManager.MainScreen.Width - (sfGameName.Width + 20), 294));
 						ScreenManager.MainScreen.Blit (sfGameGenre, new Point (ScreenManager.MainScreen.Width - (sfGameGenre.Width + 20), 330));
 						ScreenManager.MainScreen.Blit (sfGameDeveloperYear, new Point (ScreenManager.MainScreen.Width - (sfGameDeveloperYear.Width + 20), 360));
-						ScreenManager.MainScreen.Blit (itemList [itemList.Count - 1], new Point (startX, (itemY[itemList.Count - 1])));
+						ScreenManager.MainScreen.Blit (itemList [itemList.Count - 1], new Point (itemX[itemList.Count - 1], itemY[itemList.Count - 1]));
 						
 						//Load image
 						//Check roms present in rom folder and return the list with extra infos
@@ -309,35 +393,47 @@ namespace MonoFe.GameStates
 						switch (itemList.Count)
 						{
 						case 0:
-						case 14:
-							itemList.Add (fontXXSmall.Render (g.gameName, Color.White));
+						case 20:
+							itemList.Add (font8.Render (g.gameName, Color.White));
 							break;
 						case 1:
-						case 13:
-							itemList.Add (fontXSmall.Render (g.gameName, Color.White));
+						case 19:
+							itemList.Add (font10.Render (g.gameName, Color.White));
 							break;
 						case 2:
-						case 12:
-							itemList.Add (fontSmall.Render (g.gameName, Color.White));
+						case 18:
+							itemList.Add (font12.Render (g.gameName, Color.White));
 							break;
 						case 3:
-						case 11:
-							itemList.Add (fontNormal.Render (g.gameName, Color.White));
+						case 17:
+							itemList.Add (font14.Render (g.gameName, Color.White));
 							break;
 						case 4:
-						case 10:
-							itemList.Add (fontMedium.Render (g.gameName, Color.White));
+						case 16:
+							itemList.Add (font16.Render (g.gameName, Color.White));
 							break;
 						case 5:
-						case 9:
-							itemList.Add (fontLarge.Render (g.gameName, Color.White));
+						case 15:
+							itemList.Add (font18.Render (g.gameName, Color.White));
 							break;
 						case 6:
+						case 14:
+							itemList.Add (font20.Render (g.gameName, Color.White));
+							break;
+						case 7:
+						case 13:
+							itemList.Add (font24.Render (g.gameName, Color.White));
+							break;
 						case 8:
-							itemList.Add (fontXL.Render (g.gameName, Color.White));
+						case 12:
+							itemList.Add (font28.Render (g.gameName, Color.White));
+							break;
+						case 9:
+						case 11:
+							itemList.Add (font32.Render (g.gameName, Color.White));
 							break;
 						}
-						ScreenManager.MainScreen.Blit (itemList [itemList.Count - 1], new Point (startX, (itemY[itemList.Count - 1])));
+						ScreenManager.MainScreen.Blit (itemList [itemList.Count - 1], new Point (itemX[itemList.Count - 1], (itemY[itemList.Count - 1])));
 					}
 					//3 cases
 					//currentIndex < 5 must show emtpy items
@@ -521,6 +617,7 @@ namespace MonoFe.GameStates
 			//gameList = gp.filterMameRoms(gameList,ConfigurationSettings.AppSettings["MameRomFolder"]);
 				break;
 			}
+
 			currentIndex = 0;
 		}
 	}
